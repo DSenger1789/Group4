a1e7f063cbcb9eac0512e6e69982b7aa0f9f0b93
DSenger1789 <44330221+DSenger1789@users.noreply.github.com>
2019-09-19 14:55:09 -0600

    Dustin's information

--- group.c
+++ group.c
@@ -1,14 +1,24 @@
 // Group methods include name, year in school, major , hobbies, hometown, anything else interesting. 
 
+void Dustin(chars[] userName, chars[] class, chars[] major, chars[] hobbies, chars[] hometown, chars[] anythingElse){
+  userName = "Dustin Senger";
+  class = "Senior";
+  major = "Finance";
+  hobbies = "Photography";
+  hometown = "Great Falls";
+  anythingElse = "Did a 6 month tour in the Middle East w/ the Air Natl. Guard";
+  
+  printf("%s %s %s %s %s %s %s", userName, class, major, hobbies, hometown, anythingElse);
+}
 
 
 int main(){
   chars userName[20];
-  int yearInSchool;
+  chars class[15];
   chars major[15];
   chars hobbies[60];
   chars hometown[20];
   chars anythingElse[90];
-  
-  
+ 
+ return 0; 
 }
