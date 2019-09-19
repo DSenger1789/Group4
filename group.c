8e8657af7da2e3b06abaff94c9cc3bd8fbc811a4
DSenger1789 <44330221+DSenger1789@users.noreply.github.com>
2019-09-19 14:45:27 -0600

    Update group.c

--- group.c
+++ group.c
@@ -1,1 +1,14 @@
 // Group methods include name, year in school, major , hobbies, hometown, anything else interesting. 
+
+
+
+int main(){
+  chars userName[20];
+  int yearInSchool;
+  chars major[15];
+  chars hobbies[60];
+  chars hometown[20];
+  chars anythingElse[90];
+  
+  
+}
