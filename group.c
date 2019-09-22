fcf090d4f1402742c9a52bab10def70d55348abf
loonylunar8 <lslucky8@yahoo.com>
2019-09-22 16:13:54 -0600

    Edit 4 Linda

--- group.c
+++ group.c
@@ -1,10 +1,18 @@
 // Group methods include name, year in school, major , hobbies, hometown, anything else interesting. 
+#include <stdio.h>
+
 void Linda(void){
 	char myName[20] = "Linda Schimming";
 	char myYearInSchool[3] = "3";
     char myMajor[30] = "Computer Science";
 	char myHobbies[40] = "drawing, horseback riding";
     char myHometown[20] = "Puyallup,WA";
+	char myInteresting[20] = "unsure";
+  
+  printf(" My Name: %s\n My year in school: %s\n My Major: %s \n My Hobbies: %s \n My Hometown: %s \n Someting interesting : %s \n",myName, myYearInSchool, myMajor, myHobbies, myHometown, myInteresting);
+   
+  
+
 }
 
 
