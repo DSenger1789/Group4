53a6217b8a18b7bc1b1b6b7d0de9900adaee1e97
DSenger1789 <44330221+DSenger1789@users.noreply.github.com>
2019-09-23 18:42:03 -0600

    Formatting update
    Please find functioning call to individual function and functioning main()

--- group.c
+++ group.c
@@ -1,24 +1,21 @@
 // Group methods include name, year in school, major , hobbies, hometown, anything else interesting. 
+#include <stdio.h>
 
-void Dustin(chars[] userName, chars[] class, chars[] major, chars[] hobbies, chars[] hometown, chars[] anythingElse){
-  userName = "Dustin Senger";
-  class = "Senior";
-  major = "Finance";
-  hobbies = "Photography";
-  hometown = "Great Falls";
-  anythingElse = "Did a 6 month tour in the Middle East w/ the Air Natl. Guard";
-  
-  printf("%s %s %s %s %s %s %s", userName, class, major, hobbies, hometown, anythingElse);
+void Dustin(){
+  char userName[20] = "Dustin Senger";
+  char class[15]  = "Senior";
+  char major[15] = "Finance";
+  char hobbies[60]  = "Photography";
+  char hometown[20] = "Great Falls";
+  char anythingElse[100] = "Did a 6 month tour in the Middle East w/ the Air Natl. Guard";
+ 
+  printf("%s %s %s %s %s %s", userName, class, major, hobbies, hometown, anythingElse);
 }
 
 
 int main(){
-  chars userName[20];
-  chars class[15];
-  chars major[15];
-  chars hobbies[60];
-  chars hometown[20];
-  chars anythingElse[90];
- 
+
+ Dustin();
+
  return 0; 
 }
